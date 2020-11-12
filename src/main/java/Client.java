547957d76ab8c0bd48f3eb856f5c0701126131cf
tonyha <tonytaesung.ha@gmail.com>
2020-11-11 18:59:20 -0800

    Implemented a basic server using http-core which can receive and responsd to http requests. Client.java is currently not working.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,20 +1,40 @@
+import org.apache.http.*;
+import org.apache.http.impl.DefaultBHttpClientConnection;
+import org.apache.http.impl.DefaultBHttpClientConnectionFactory;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.util.EntityUtils;
+
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.InetAddress;
+import java.net.Socket;
+
 
 public class Client {
-    public static void sendGetRequest() throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:6666"))
-                .build();
-
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-
-        System.out.println(response.body());
+    public static void sendGetRequest() throws IOException, InterruptedException, HttpException {
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create("http://localhost:6666"))
+//                .build();
+//
+//        HttpResponse<String> response = client.send(request,
+//                HttpResponse.BodyHandlers.ofString());
+//        System.out.println(response.body());
+        InetAddress host = InetAddress.getLocalHost();
+        Socket socket = new Socket(host, 8080);
+        System.out.println("opened socket on port 6666");
+        DefaultBHttpClientConnection conn = new DefaultBHttpClientConnection(1024);
+        conn.bind(socket);
+        HttpRequest request = new BasicHttpRequest("GET", "/");
+        System.out.println("sending request header");
+        conn.sendRequestHeader(request);
+        System.out.println("waiting for response");
+        HttpResponse response = conn.receiveResponseHeader();
+        conn.receiveResponseEntity(response);
+        System.out.println("received response");
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            EntityUtils.consume(entity);
+        }
     }
 
     public static void main(String[] args) {
@@ -23,6 +43,8 @@ public class Client {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        } catch (HttpException e) {
             System.out.println(e.getMessage());
         }
     }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,55 +1,105 @@
 import org.apache.http.*;
-import org.apache.http.impl.DefaultHttpResponseFactory;
-import org.apache.http.impl.io.DefaultHttpRequestParser;
-import org.apache.http.impl.io.HttpTransportMetricsImpl;
-import org.apache.http.impl.io.SessionInputBufferImpl;
-import org.apache.http.io.HttpMessageParser;
-import org.apache.http.io.HttpTransportMetrics;
-import org.apache.http.io.SessionInputBuffer;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.HttpCoreContext;
+import org.apache.http.config.SocketConfig;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.bootstrap.HttpServer;
+import org.apache.http.impl.bootstrap.ServerBootstrap;
+import org.apache.http.protocol.*;
 
-
-import java.net.*;
 import java.io.*;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
     public void start(int port) {
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            String inputLine;
+            HttpRequestHandler requestHandler = new HttpRequestHandler() {
+                @Override
+                public void handle(HttpRequest httpRequest, HttpResponse httpResponse, HttpContext httpContext) throws HttpException, IOException {
+                    System.out.println("handling request");
+                }
+            };
 
-            SessionInputBufferImpl inBuffer = new SessionInputBufferImpl(
-                    new HttpTransportMetricsImpl(), 256);
-            inBuffer.bind(clientSocket.getInputStream());
+            List<HttpRequestInterceptor> requestIntercepts = new ArrayList<HttpRequestInterceptor>();
+            List<HttpResponseInterceptor> responseIntercepts = new ArrayList<HttpResponseInterceptor>();
 
-            HttpMessageParser<HttpRequest> requestParser = new DefaultHttpRequestParser(inBuffer);
-            HttpRequest request = requestParser.parse();
-            HeaderIterator iterator = request.headerIterator();
+            requestIntercepts.add(new HttpRequestInterceptor() {
+                @Override
+                public void process(HttpRequest httpRequest, HttpContext httpContext) throws HttpException, IOException {
+                    Header[] headers = httpRequest.getAllHeaders();
 
-            System.out.println("request headers:");
+                    System.out.println("receiving http request " + httpRequest);
+                    for (int i = 0; i < headers.length; i++) {
+                        Header header = headers[i];
+                        HeaderElement[] headerElements = header.getElements();
 
-            while (iterator.hasNext()) {
-                Header header = iterator.nextHeader();
-                System.out.printf("%s: %s\n", header.getName(), header.getValue());
-            }
+                        System.out.println("headers:");
+                        for (int j = 0; j < headerElements.length; j++) {
+                            HeaderElement headerElement = headerElements[j];
+                            System.out.printf("%s : %s \n", headerElement.getName(), headerElement.getValue());
+                        }
+                    }
+                }
+            });
+
+            responseIntercepts.add(new HttpResponseInterceptor() {
+                @Override
+                public void process(HttpResponse httpResponse, HttpContext httpContext) throws HttpException, IOException {
+                    StatusLine status = httpResponse.getStatusLine();
+                    System.out.printf("sending out request | protocol: %s | status: %d - %s", status.getProtocolVersion(),
+                            status.getStatusCode(), status.getReasonPhrase());
+                }
+            });
+
+            HttpProcessor httpProcessor = new ImmutableHttpProcessor(requestIntercepts, responseIntercepts);
+
+            SocketConfig config = SocketConfig.custom()
+                    .setSoTimeout(15000)
+                    .setTcpNoDelay(true)
+                    .build();
+
+            InetAddress hostAddress = InetAddress.getLocalHost();
+
+            final HttpServer server = ServerBootstrap.bootstrap()
+                    .setLocalAddress(hostAddress)
+                    .setListenerPort(port)
+                    .setHttpProcessor(httpProcessor)
+                    .setSocketConfig(config)
+                    .registerHandler("/*", requestHandler)
+                    .create();
+
+            System.out.println("initialized server");
+
+            server.start();
+            System.out.println("started server on port " + port);
+            server.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    server.shutdown(5, TimeUnit.SECONDS);
+                }
+            });
         } catch (IOException e) {
+            System.out.println("IO exception");
             System.out.println(e.getMessage());
-        } catch (HttpException e) {
+        } catch (InterruptedException e) {
+            System.out.println("Interupted exception");
             System.out.println(e.getMessage());
         }
     }
 
-    public void generateResponse() {
-        ProtocolVersion http = new ProtocolVersion("HTTP", 1, 1);
-        HttpContext context = new HttpCoreContext();
-        DefaultHttpResponseFactory responseFactory = new DefaultHttpResponseFactory();
-        HttpResponse resp = responseFactory.newHttpResponse(http, 200, context);
+    public void printHeaders(HttpRequest req) {
+        HeaderIterator iterator = req.headerIterator();
+
+        while (iterator.hasNext()) {
+            Header header = iterator.nextHeader();
+            System.out.printf("%s: %s\n", header.getName(), header.getValue());
+        }
     }
 
     public static void main(String[] args) {
-        new Server().start(6666);
+        new Server().start(8080);
     }
 }
