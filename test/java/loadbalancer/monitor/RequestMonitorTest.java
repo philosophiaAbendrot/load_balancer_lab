3f0adf4bebb49de15ee73a94fc173ab8a5327728
tonytaesung.ha@gmail.com <tonytaesung.ha@gmail.com>
2021-03-13 22:59:20 -0800

    Finished writing tests for RequestMonitor.

--- test/java/loadbalancer/monitor/RequestMonitorTest.java
+++ test/java/loadbalancer/monitor/RequestMonitorTest.java
@@ -9,61 +9,85 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RequestMonitorTest {
     private RequestMonitor reqMonitor;
+    private List<List<Long>> reqData;
 
     @BeforeEach
     public void setupTest() {
         this.reqMonitor = new RequestMonitor("RequestMonitorTest");
+        this.reqData = new ArrayList<>();
     }
 
     // test that request timestamps are recorded
     @Test
     @DisplayName("Test that request timestamps recorded in RequestMonitor are correct")
     public void checkRequestTimestampsRecorded() {
-        List<List<Long>> requestData = new ArrayList<>();
-        long indexTime = System.currentTimeMillis();
-
-        requestData.add(new ArrayList<>(Arrays.asList(0L, 200L)));
-        requestData.add(new ArrayList<>(Arrays.asList(300L, 400L)));
-        requestData.add(new ArrayList<>(Arrays.asList(650L, 750L)));
-        requestData.add(new ArrayList<>(Arrays.asList(800L, 850L)));
-
-        List<List<Long>> requestDataOffset = new ArrayList<>();
-
-        for (List<Long> datum : requestData) {
-            List<Long> newDatum = new ArrayList<>();
-            newDatum.add(datum.get(0) + indexTime);
-            newDatum.add(datum.get(1) + indexTime);
-            requestDataOffset.add(newDatum);
-        }
-
-        // add data to request monitor
-        for (List<Long> datum : requestDataOffset)
-            this.reqMonitor.addRecord(datum.get(0), datum.get(1));
-
+        setupDefaultTestCase();
 
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {  }
 
         double activeTime = 0;
-        long totalTime = System.currentTimeMillis() - requestDataOffset.get(0).get(0);
+        double totalTime = (double)(System.currentTimeMillis() - this.reqData.get(0).get(0));
 
-        for (List<Long> datum : requestData)
+        for (List<Long> datum : this.reqData)
             activeTime += (datum.get(1) - datum.get(0));
 
         double expectedCapacityFactor = activeTime / totalTime;
         double actualCapacityFactor = this.reqMonitor.getCapacityFactor();
-        double percentDifference = Math.abs(actualCapacityFactor - expectedCapacityFactor) / ((actualCapacityFactor + expectedCapacityFactor) / 2);
-        assertTrue(percentDifference < 0.1, "Expected and actual capacity factor is different");
+        assertTrue(percentageDifference(actualCapacityFactor, expectedCapacityFactor) < 10, "Expected and actual capacity factor is different");
     }
 
-    // test that number of requests recorded is correct
+    // test that number of requests recorded is correct after the old data is cleared out
+    @Test
+    @DisplayName("Test that older request timestamps are cleared out in capacityFactor calculation")
+    public void checkNumRequests() {
+        setupDefaultTestCase();
 
-    // test that capacity factor is correct
+        // wait for older request data to clear out
+        try {
+            Thread.sleep(11_000);
+        } catch (InterruptedException e) { }
 
-    // test that data is cleared out
+        long newIndexTime = System.currentTimeMillis();
+
+        this.reqData.add(new ArrayList<>(Arrays.asList(newIndexTime + 300L, newIndexTime + 400L)));
+        this.reqData.add(new ArrayList<>(Arrays.asList(newIndexTime + 500L, newIndexTime + 600L)));
+        this.reqData.add(new ArrayList<>(Arrays.asList(newIndexTime + 650L, newIndexTime + 690L)));
+        this.reqData.add(new ArrayList<>(Arrays.asList(newIndexTime + 780L, newIndexTime + 840L)));
+
+        double totalTime = (double)(System.currentTimeMillis() - (newIndexTime + 300L));
+        double activeTime = 0;
+
+        for (int i = 4; i < 8; i++) {
+            List<Long> datum = this.reqData.get(i);
+            activeTime += (double)(datum.get(1) - datum.get(0));
+        }
+
+        double expectedCapacityFactor = activeTime / totalTime;
+        double actualCapacityFactor = this.reqMonitor.getCapacityFactor();
+
+        assertTrue(percentageDifference(expectedCapacityFactor, actualCapacityFactor) < 10, "Expected and actual capacity factor is different");
+    }
+
+    private void setupDefaultTestCase() {
+        List<List<Long>> requestData = new ArrayList<>();
+        long indexTime = System.currentTimeMillis();
+
+        this.reqData.add(new ArrayList<>(Arrays.asList(indexTime, 200L + indexTime)));
+        this.reqData.add(new ArrayList<>(Arrays.asList(300L + indexTime, 400L + indexTime)));
+        this.reqData.add(new ArrayList<>(Arrays.asList(650L + indexTime, 750L + indexTime)));
+        this.reqData.add(new ArrayList<>(Arrays.asList(800L + indexTime, 850L + indexTime)));
+
+        // add data to request monitor
+        for (List<Long> datum : this.reqData)
+            this.reqMonitor.addRecord(datum.get(0), datum.get(1));
+    }
+
+    private double percentageDifference(double a, double b) {
+        return Math.abs(a - b) / ((a + b) / 2.0) * 100;
+    }
 }
