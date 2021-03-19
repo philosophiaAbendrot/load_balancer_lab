11d50b25872acc3b894dbba0e1b1c7058d5ac972
tonyha <tonytaesung.ha@gmail.com>
2021-03-18 23:45:49 -0700

    Made test on BackEndInitiator checking that BackEndInitiator creates BackEnd instances when sent a request to do so.

--- test/java/BackEndInitiatorTest.java
+++ test/java/BackEndInitiatorTest.java
@@ -5,23 +5,32 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.apache.http.impl.client.HttpClients;
+import org.mockito.Mockito;
 
 import loadbalancer.BackEndInitiator;
 import loadbalancer.factory.BackEndFactoryImpl;
 import loadbalancer.factory.BackEndFactory;
 import loadbalancer.util.Logger;
-import org.mockito.Mockito;
+import loadbalancer.monitor.RequestMonitor;
+import loadbalancer.BackEnd;
 
 import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class BackEndInitiatorTest {
     @Test
     @DisplayName("BackEndInitiator should start up a BackEnd instance when sent a request telling it to do so")
     public void BackEndInitiatorShouldStartBackEndInstance() {
         Logger.configure(new String[]{"capacityModulation"});
+        BackEndFactory mockFactory = Mockito.mock(BackEndFactoryImpl.class);
+        BackEnd backEnd = new BackEnd(new RequestMonitor("BackEndInitiatorTest"));
 
-        BackEndFactory factory = Mockito.mock(BackEndFactoryImpl.class);
-        BackEndInitiator initiator = new BackEndInitiator(factory);
+        when(mockFactory.produceBackEnd(any(RequestMonitor.class))).thenReturn(backEnd);
+        when(mockFactory.produceBackEndThread(any(BackEnd.class))).thenReturn(new Thread(backEnd));
+
+        BackEndInitiator initiator = new BackEndInitiator(mockFactory);
         Thread initiatorThread = new Thread(initiator);
         initiatorThread.start();
 
@@ -38,21 +47,14 @@ public class BackEndInitiatorTest {
         HttpPost req = new HttpPost("http://127.0.0.1:" + backEndInitiatorPort + "/backends");
 
         try {
-            Thread.sleep(3_000);
-        } catch (InterruptedException e) {
-            System.out.println("Interrupted Exception thrown while waiting for BackEndInitiator to start up");
+            CloseableHttpResponse response = client.execute(req);
+            client.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        try {
-            System.out.println("sending request to BackEndInitiator");
-            CloseableHttpResponse response = client.execute(req);
-        } catch (IOException e) { e.printStackTrace(); }
-    }
-
-    // send request to BackEndInitiator
-    private void sendRequest() {
-
+        verify(mockFactory, times(1)).produceBackEnd(any(RequestMonitor.class));
+        verify(mockFactory, times(1)).produceBackEndThread(any(BackEnd.class));
     }
 
     // backend initiator should shutdown a backend server when sent a request telling it to do so
