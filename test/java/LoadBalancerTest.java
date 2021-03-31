364e351dbd52f4fae9f134063ca8d88b7e28e888
tonytaesung.ha@gmail.com <tonytaesung.ha@gmail.com>
2021-03-30 19:11:55 -0700

    Created empty class to store LoadBalancer tests.

---
+++ test/java/LoadBalancerTest.java
@@ -0,0 +1,7 @@
+public class LoadBalancerTest {
+    // check that load balancer starts up a CapacityFactorImpl instance
+
+    // check that load balancer sends requests to start up x backend server threads immediately following startup
+
+    // test that requests to LoadBalancer are passed on to BackEnds
+}
