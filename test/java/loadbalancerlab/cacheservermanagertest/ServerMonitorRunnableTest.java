498b4cfe886437eb21ea4585eeabb3bce589bf2a
tonytaesung.ha@gmail.com <tonytaesung.ha@gmail.com>
2021-05-29 10:48:44 -0700

    Implemented tests for deliverData on ServerMonitorRunnableTest.

--- test/java/loadbalancerlab/cacheservermanagertest/ServerMonitorRunnableTest.java
+++ test/java/loadbalancerlab/cacheservermanagertest/ServerMonitorRunnableTest.java
@@ -1,19 +1,11 @@
 package loadbalancerlab.cacheservermanagertest;
 
-import loadbalancerlab.cacheserver.CacheServer;
 import loadbalancerlab.cacheservermanager.CacheServerManager;
-import loadbalancerlab.cacheservermanager.ServerMonitor;
 import loadbalancerlab.cacheservermanager.ServerMonitorRunnable;
 import loadbalancerlab.factory.HttpClientFactory;
-import loadbalancerlab.factory.HttpClientFactoryImpl;
-import loadbalancerlab.services.monitor.CapacityFactorMonitor;
-import loadbalancerlab.services.monitor.CapacityFactorMonitorImpl;
 import loadbalancerlab.util.Logger;
 import loadbalancerlab.util.RequestDecoder;
-import loadbalancerlab.util.RequestDecoderImpl;
-
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -26,16 +18,20 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import java.io.IOException;
+import java.util.SortedMap;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
 
 public class ServerMonitorRunnableTest {
     HttpClientFactory clientFactory;
     CloseableHttpClient mockClient;
     long currentTime;
+    ServerMonitorRunnable serverMonitorRunnable;
+    CacheServerManager mockCacheServerManager;
+    RequestDecoder mockDecoder;
 
     @BeforeEach
     public void setup() {
@@ -43,21 +39,47 @@ public class ServerMonitorRunnableTest {
         this.clientFactory = Mockito.mock(HttpClientFactory.class);
         this.mockClient = Mockito.mock(CloseableHttpClient.class);
         this.currentTime = System.currentTimeMillis();
+        this.mockDecoder = Mockito.mock(RequestDecoder.class);
+        this.mockCacheServerManager = Mockito.mock(CacheServerManager.class);
     }
 
     @Nested
-    @DisplayName("Testing server analytics")
-    public class TestServerAnalytics {
+    @DisplayName("Testing deliverData()")
+    public class TestDeliverData {
+        @BeforeEach
+        public void setup() {
+            serverMonitorRunnable = new ServerMonitorRunnable(clientFactory, mockDecoder, ServerMonitorRunnableTest.this.mockCacheServerManager);
+        }
+
         @Test
         @DisplayName("Should return the correct number of active servers by second")
         public void returnActiveServers() {
+            int currentTime = (int)System.currentTimeMillis() / 1_000;
+            serverMonitorRunnable.updateServerCount(currentTime + 1, 10);
+            serverMonitorRunnable.updateServerCount(currentTime + 2, 12);
+            serverMonitorRunnable.updateServerCount(currentTime + 3, 15);
+            SortedMap<Integer, Integer> serverData = serverMonitorRunnable.deliverData();
 
+            assertTrue(serverData.containsKey(currentTime + 1));
+            assertTrue(serverData.containsKey(currentTime + 2));
+            assertTrue(serverData.containsKey(currentTime + 3));
+            assertEquals(serverData.get(currentTime + 1), 10);
+            assertEquals(serverData.get(currentTime + 2), 12);
+            assertEquals(serverData.get(currentTime + 3), 15);
         }
 
         @Test
         @DisplayName("If there are duplicate entries for a given second, the first one should be recorded")
         public void ignoreDuplicateInputs() {
+            int currentTime = (int)System.currentTimeMillis() / 1_000;
+            serverMonitorRunnable.updateServerCount(currentTime + 1, 10);
+            serverMonitorRunnable.updateServerCount(currentTime + 2, 12);
+            serverMonitorRunnable.updateServerCount(currentTime + 2, 16);
+            serverMonitorRunnable.updateServerCount(currentTime + 3, 15);
+            SortedMap<Integer, Integer> serverData = serverMonitorRunnable.deliverData();
 
+            assertTrue(serverData.containsKey(currentTime + 2));
+            assertEquals(serverData.get(currentTime + 2), 12);
         }
     }
 
@@ -66,16 +88,13 @@ public class ServerMonitorRunnableTest {
     class TestPingCacheServers {
         ArgumentCaptor<HttpUriRequest> argument;
         int cacheServerPort;
-        ServerMonitorRunnable serverMonitorRunnable;
         JSONObject mockJsonResponse;
-        RequestDecoder mockDecoder;
         CloseableHttpResponse mockResponse;
         CacheServerManager mockCacheServerManager;
         int numServers = 3;
 
         @BeforeEach
         public void setup() throws IOException {
-            this.mockDecoder = Mockito.mock(RequestDecoder.class);
             this.mockCacheServerManager = Mockito.mock(CacheServerManager.class);
             this.argument = ArgumentCaptor.forClass(HttpGet.class);
 
@@ -85,30 +104,29 @@ public class ServerMonitorRunnableTest {
             this.mockJsonResponse = new JSONObject();
             this.mockJsonResponse.put("port", this.cacheServerPort);
             this.mockJsonResponse.put("capacity_factor", 0.5);
-            when(ServerMonitorRunnableTest.this.clientFactory.buildApacheClient()).thenReturn(ServerMonitorRunnableTest.this.mockClient);
+            when(clientFactory.buildApacheClient()).thenReturn(mockClient);
             this.mockResponse = Mockito.mock(CloseableHttpResponse.class);
-            when(ServerMonitorRunnableTest.this.mockClient.execute(any(HttpUriRequest.class))).thenReturn(this.mockResponse);
-            when(this.mockDecoder.extractJsonApacheResponse(any(CloseableHttpResponse.class))).thenReturn(this.mockJsonResponse);
-
-            this.serverMonitorRunnable = new ServerMonitorRunnable(clientFactory, mockDecoder, mockCacheServerManager);
+            when(mockClient.execute(any(HttpUriRequest.class))).thenReturn(this.mockResponse);
+            when(mockDecoder.extractJsonApacheResponse(any(CloseableHttpResponse.class))).thenReturn(this.mockJsonResponse);
+            serverMonitorRunnable = new ServerMonitorRunnable(clientFactory, mockDecoder, mockCacheServerManager);
 
             // add server info instances to monitor
             for (int i = 0; i < numServers; i++)
-                this.serverMonitorRunnable.addServer(i, 10_000 + i);
+                serverMonitorRunnable.addServer(i, 10_000 + i);
 
-            this.serverMonitorRunnable.pingCacheServers(System.currentTimeMillis() / 1_000);
+            serverMonitorRunnable.pingCacheServers(System.currentTimeMillis() / 1_000);
         }
 
         @Test
         @DisplayName("Test that request is sent to CacheServer for update on capacity factor")
         public void shouldSendRequestWithPostMethod() throws IOException {
-            verify(ServerMonitorRunnableTest.this.mockClient, times(numServers)).execute(any(HttpGet.class));
+            verify(mockClient, times(numServers)).execute(any(HttpGet.class));
         }
 
         @Test
         @DisplayName("Request should be of type POST")
         public void shouldSendRequestToCacheServerManager() throws IOException {
-            verify(ServerMonitorRunnableTest.this.mockClient, times(numServers)).execute(this.argument.capture());
+            verify(mockClient, times(numServers)).execute(this.argument.capture());
             HttpUriRequest request = this.argument.getAllValues().get(1);
             assertEquals("GET", request.getMethod());
         }
@@ -116,7 +134,7 @@ public class ServerMonitorRunnableTest {
         @Test
         @DisplayName("Request should go to correct URI")
         public void requestShouldGoToCorrectURI() throws IOException {
-            verify(ServerMonitorRunnableTest.this.mockClient, times(numServers)).execute(this.argument.capture());
+            verify(mockClient, times(numServers)).execute(this.argument.capture());
             HttpUriRequest request1 = this.argument.getAllValues().get(0);
             assertEquals("http://127.0.0.1:" + 10000 + "/capacity-factor", request1.getURI().toString());
             HttpUriRequest request2 = this.argument.getAllValues().get(1);
