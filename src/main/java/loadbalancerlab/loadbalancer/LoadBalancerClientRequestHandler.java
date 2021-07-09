ef925614dcc73a36c0cb2624566efef0f11cbd7a
Tony Ha <eothel@gmail.com>
2021-07-09 15:53:44 -0700

    Added comments to various classes in loadbalancer package.

--- src/main/java/loadbalancerlab/loadbalancer/LoadBalancerClientRequestHandler.java
+++ src/main/java/loadbalancerlab/loadbalancer/LoadBalancerClientRequestHandler.java
@@ -37,38 +37,67 @@ public class LoadBalancerClientRequestHandler implements HttpRequestHandler {
      */
     private CacheRedistributor cacheRedis;
 
+    /**
+     * Factory used for generating CloseableHttpClient instances to send HttpRequests
+     */
     public static HttpClientFactory clientFactory;
+
+    /**
+     * Logger object used for logging
+     */
     private Logger logger;
 
+    /**
+     * Configuration method used for configuring static fields
+     * @param config: Config object used to configure various classes
+     */
     public static void configure( Config config ) {
         clientFactory = config.getHttpClientFactory();
     }
 
-    public LoadBalancerClientRequestHandler( CacheRedistributor _cacheRedis) {
+    /**
+     * @param cacheRedis    CacheRedistributor object used to manage consistent hashing mechanism to allocate client
+     *                      requests to CacheServer instances
+     */
+    public LoadBalancerClientRequestHandler( CacheRedistributor cacheRedis ) {
         incomingRequestTimestamps = Collections.synchronizedList(new LinkedList<>());
-        cacheRedis = _cacheRedis;
+        this.cacheRedis = cacheRedis;
         logger = new Logger("LoadBalancerClientRequestHandler");
     }
 
+    /**
+     * Method from HttpRequestHandler interface. Used to receive and generate response to Http requests from clients.
+     * @param httpRequest       HttpRequest object which represents Http request from client
+     * @param httpResponse      HttpResponse object which represents response which will be sent back to the client
+     * @param httpContext       HttpContext object which represents execution state of an Http process
+     */
     @Override
     public void handle( HttpRequest httpRequest, HttpResponse httpResponse, HttpContext httpContext) {
         CloseableHttpClient httpClient = clientFactory.buildApacheClient();
         String uri = httpRequest.getRequestLine().getUri();
         String[] uriArr = uri.split("/");
         String resourceName = uriArr[uriArr.length - 1];
+
+        /* select port to forward request to using consistent hashing mechanism */
         int cacheServerPort = cacheRedis.selectPort(resourceName);
         logger.log(String.format("relaying message to cache server at port %d", cacheServerPort), Logger.LogType.REQUEST_PASSING);
-        // record request incoming timestamp
+
+        /* record request incoming timestamp */
         incomingRequestTimestamps.add((int)(System.currentTimeMillis() / 1000));
         HttpGet getReq = new HttpGet("http://127.0.0.1:" + cacheServerPort + "/" + resourceName);
 
         try {
+
+            /* Forward request to selected CacheServer instance */
             CloseableHttpResponse res = httpClient.execute(getReq);
+
+            /* Send back response received from from CacheServer */
             HttpEntity resBody = res.getEntity();
             httpResponse.setEntity(resBody);
             httpResponse.setStatusCode(200);
         } catch (IOException e) {
-            // if cache server failed
+
+            /* if cache server failed */
             e.printStackTrace();
             JSONObject outputJsonObj = new JSONObject();
             outputJsonObj.put("error_message", "Cache server failed to respond");
--- src/main/java/loadbalancerlab/loadbalancer/LoadBalancerRunnable.java
+++ src/main/java/loadbalancerlab/loadbalancer/LoadBalancerRunnable.java
@@ -4,31 +4,70 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 
+/**
+ * LoadBalancer object which handles delegation and forwarding of incoming client Http requests to CacheServer objects
+ */
 public class LoadBalancerRunnable implements Runnable {
-    private int port;
+    /**
+     * Port which associated CacheServerManager object is running on
+     */
     private int cacheServerManagerPort;
+
+    /**
+     * A Thread object which runs a server which handles Http requests clients
+     */
     private Thread clientReqHandlerThread;
+
+    /**
+     * Server which handles Http requests from clients
+     */
     private ClientRequestHandlerServer clientReqHandlerRunnable;
+
+    /**
+     * LoadBalancerClientRequestHandler object which contains the logic to handle Http requests from clients.
+     * Inserted into 'clientReqHandlerRunnable' object upon creation
+     */
     private LoadBalancerClientRequestHandler clientReqHandler;
+
+    /**
+     * CacheRedistributor object which manages the dynamic-redistribution of resource names among CacheServer objects.
+     * Acts as a middleman between this object and a HashRing object which handles a consistent hashing mechanism.
+     */
     private CacheRedistributor cacheRedis;
-    private Thread cacheRedisThread;
+
+    /**
+     * HashRing object which handles consistent hashing mechanism for mapping resource names to CacheServers
+     */
     private HashRing hashRing;
 
-    public LoadBalancerRunnable(int _cacheServerManagerPort) {
-        cacheServerManagerPort = _cacheServerManagerPort;
+    /**
+     * Thread instance which runs 'cacheRedis' object
+     */
+    private Thread cacheRedisThread;
 
-        // prepare CacheRedistributor
+    /**
+     * @param cacheServerManagerPort        Port which associated CacheServerManager object is running on
+     */
+    public LoadBalancerRunnable(int cacheServerManagerPort) {
+        this.cacheServerManagerPort = cacheServerManagerPort;
+
+        /* Prepare CacheRedistributor */
         hashRing = new HashRing();
-        cacheRedis = new CacheRedistributor(cacheServerManagerPort, hashRing);
+        cacheRedis = new CacheRedistributor(this.cacheServerManagerPort, hashRing);
         CacheRedistributorRunnable cacheRedisRunnable = new CacheRedistributorRunnable(cacheRedis);
         cacheRedisThread = new Thread(cacheRedisRunnable);
 
-        // prepare ClientRequestHandler server
+        /* Prepare ClientRequestHandler server */
         clientReqHandler = new LoadBalancerClientRequestHandler(cacheRedis);
         clientReqHandlerRunnable = new ClientRequestHandlerServer(clientReqHandler);
         clientReqHandlerThread = new Thread(clientReqHandlerRunnable);
     }
 
+    /**
+     * Getter method for CacheRedistributor.angleHistory field, which stores snapshots of a table mapping CacheServer ids
+     * to HashRingAngle instances that belong to them.
+     * @return: A table which maps timestamps to snapshots of CacheServer to HashRingAngle mappings at those times
+     */
     public SortedMap<Integer, Map<Integer, List<HashRingAngle>>> getHashRingAngleHistory() {
         return cacheRedis.getHashRingAngleHistory();
     }
